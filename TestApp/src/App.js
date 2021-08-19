c4908813d20a04c0dd350aaf36d13cd7679190ea
weijiepris <altxmare@hotmail.com>
2021-08-19 21:56:52 +0800

    firestore
    added function to store data into firestore

--- TestApp/src/App.js
+++ TestApp/src/App.js
@@ -1,6 +1,30 @@
 import * as XLSX from "xlsx";
 
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCJO9dx2KjDfQvDHkpoxG3kZ49QyeiJGTc",
+  authDomain: "bigdocker-csci321.firebaseapp.com",
+  projectId: "bigdocker-csci321",
+  storageBucket: "bigdocker-csci321.appspot.com",
+  messagingSenderId: "258512757467",
+  appId: "1:258512757467:web:18fd0326ed6097963a62fe",
+};
+
+// Initialize Firebase
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig);
+}
+
 function App() {
+  const upload = () => {
+    console.log("test");
+    firebase.firestore().collection("users").doc("data").set({
+      test: "sample data",
+    });
+  };
+
+
   const readExcel = (file) => {
     const promise = new Promise((resolve, reject) => {
       const fileReader = new FileReader();
@@ -30,14 +54,17 @@ function App() {
     });
   };
   return (
-    <input
-      type="file"
-      onChange={(e) => {
-        const file = e.target.files[0];
+    <div>
+      <input
+        type="file"
+        onChange={(e) => {
+          const file = e.target.files[0];
 
-        readExcel(file);
-      }}
-    />
+          readExcel(file);
+        }}
+      />
+      <input type="button" onClick={upload} />
+    </div>
   );
 }
 
