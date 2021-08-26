19c5b2da7744469187a7b7c422d9a10fb64db390
weijiepris <altxmare@hotmail.com>
2021-08-27 00:17:42 +0800

    sage cells
    -

--- ipynb/src/App.js
+++ ipynb/src/App.js
@@ -1,7 +1,30 @@
-import * as XLSX from "xlsx";
-
 function App() {
-  return <div></div>
+  const add = () => {
+  };
+
+  const elements = ["one", "two", "three"];
+  return (
+    <div id="test">
+      Type your own Sage computation below and click “Evaluate”.
+      <div className="compute">
+        <script type="text/x-sage"></script>
+      </div>
+      <div className="compute">
+        <script type="text/x-sage">
+          @interact def f(n=(0,10)): print(2^n)
+        </script>
+      </div>
+      {elements.map((value, index) => {
+        return (
+          <div className="compute">
+            <script type="text/x-sage"></script>
+          </div>
+        );
+      })}
+      <br></br>
+      <button onClick={add}>Add new cell</button>
+    </div>
+  );
 }
 
 export default App;
--- ipynb/src/index.js
+++ ipynb/src/index.js
@@ -1,11 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import JupyterViewer from "react-jupyter-notebook";
-import nb_test from "./nb_test.json"; // You need to read the .ipynb file into a JSON Object.
-
-ReactDOM.render(
-  <React.StrictMode>
-    <JupyterViewer rawIpynb={nb_test}/>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+ReactDOM.render(<App />, document.getElementById("root"));
