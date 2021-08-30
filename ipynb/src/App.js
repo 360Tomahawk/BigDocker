d9b310fbc555ad2ae5678d8d8f4942c42bc597dd
weijiepris <altxmare@hotmail.com>
2021-08-30 23:10:20 +0800

    added simple UI

--- ipynb/src/App.js
+++ ipynb/src/App.js
@@ -1,29 +1,18 @@
+import React, { useState } from "react";
+import Main from "./components/Main";
+import SageCells from "./components/SageCells";
+import { Switch, Route } from "react-router-dom";
 function App() {
-  const add = () => {
-  };
-
-  const elements = ["one", "two", "three"];
   return (
-    <div id="test">
-      Type your own Sage computation below and click “Evaluate”.
-      <div className="compute">
-        <script type="text/x-sage"></script>
-      </div>
-      <div className="compute">
-        <script type="text/x-sage">
-          @interact def f(n=(0,10)): print(2^n)
-        </script>
-      </div>
-      {elements.map((value, index) => {
-        return (
-          <div className="compute">
-            <script type="text/x-sage"></script>
-          </div>
-        );
-      })}
-      <br></br>
-      <button onClick={add}>Add new cell</button>
-    </div>
+    <Switch>
+      <Route path="/" exact>
+        <Main />
+      </Route>
+
+      <Route path="/SageCells">
+        <SageCells />
+      </Route>
+    </Switch>
   );
 }
 
