f29c328e9e1c8a18bddd106d4eb10be197f28cbc
weijiepris <altxmare@hotmail.com>
2021-09-06 14:37:38 +0800

    -

--- sage/src/components/SageCells.js
+++ sage/src/components/SageCells.js
@@ -12,24 +12,32 @@ const SageCells = () => {
   const cell = () => {
     var div = document.createElement("div");
     div.setAttribute("class", "compute");
+    div.setAttribute("id", "testing");
     document.getElementById("test").appendChild(div);
   };
   useEffect(() => {
+    let down = document.getElementById("testing");
+    down.innerhtml = "";
+    down.removeAttribute("class");
+    down.setAttribute("class", "compute");
+    document.getElementById("test").appendChild(down);
+
     window.sagecell.makeSagecell({
       inputLocation: "div.compute",
       evalButtonText: "Evaluate",
       linked: true,
     });
     console.log("called");
+    
+    down.innerhtml = "";
   }, [cells]);
 
   return (
     <React.Fragment>
       <div id="test">
         Type your own Sage computation below and click “Evaluate”.
-        <div class="compute">
-        </div>
-        <br></br>
+        <div class="compute" id="testing"></div>
+        <div class="compute" id="testing"></div>
       </div>
       <button onClick={add}>Add new cell</button>
     </React.Fragment>
