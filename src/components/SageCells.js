b6b1f443ef852d77d0a7d16eeabbb854a1959c16
Zheng Qingping <qingping1998@gmail.com>
2021-09-07 17:19:57 +0800

    Update SageCells.js
    refactored almost the whole system, we hackermans now

--- src/components/SageCells.js
+++ src/components/SageCells.js
@@ -5,27 +5,39 @@ export default class SageCells extends React.Component{
   constructor() {
     super();
     this.state = {
-      numCells: 0, //default number of cells??
+      cellPos: 0 //default number of cells??
     }
   }
 
-  addCell = () => {
-    
-    var div = document.createElement("div");
-    div.setAttribute("class", "compute");
-    document.getElementById("cellHolder").appendChild(div);
+  spawnCells = (numberOfCells) => {
+    for (let i = 0; i < numberOfCells; i++) {
+      var div = document.createElement("div");
+      div.setAttribute("class", "compute");
+      div.style.display = "none";
+      document.getElementById("cellHolder").appendChild(div);
+    }
 
     window.sagecell.makeSagecell({inputLocation: 'div.compute',
                         evalButtonText: 'Evaluate',
                         linked: true});
+  }
 
-    this.setState({numCells: this.state.numCells + 1});
+  componentDidMount() {
+    this.spawnCells(100);
+    var c = document.getElementById("cellHolder").childElementCount;
+    console.log("number of cells created: " + c);
+  }
+
+  addCell = () => {
+    var cellHolder = document.getElementById("cellHolder").children;
+    cellHolder[this.state.cellPos].style.display = "block";
+    this.setState({cellPos: this.state.cellPos + 1});
   }
 
   deleteCell = (cellAtPos) => {
-      this.setState({numCells: this.state.numCells - 1});
-      window.sagecell.deleteSagecell(this.state.cellInfo[cellAtPos]);
-      this.state.cellInfo.splice(cellAtPos, 1);
+      this.setState({cellPos: this.state.cellPos - 1});
+      //window.sagecell.deleteSagecell(this.state.cellInfo[cellAtPos]);
+      //this.state.cellInfo.splice(cellAtPos, 1);
   }
 
   render() {
