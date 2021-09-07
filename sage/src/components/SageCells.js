def44da903f4970b66343ee34c40f3f5a8695dc8
weijiepris <altxmare@hotmail.com>
2021-09-07 15:07:39 +0800

    able to 'add' new cells

--- sage/src/components/SageCells.js
+++ sage/src/components/SageCells.js
@@ -2,44 +2,50 @@ import classes from "../css/Main.module.css";
 import React, { useState, useEffect } from "react";
 
 const SageCells = () => {
-  const [cells, setCells] = useState(1);
+  // useState(2) means the initialised value is 2
+  const [cellPos, setCellPos] = useState(2);
 
   const add = () => {
-    setCells((cells) => cells + 1);
-    cell();
-    console.log(cells);
+    let nodes = document.getElementById("mainDiv").children;
+    // set the display to block based on the index
+    nodes[cellPos].style.display = "block";
+    // increase the index by 1
+    setCellPos(cellPos + 1);
   };
-  const cell = () => {
-    var div = document.createElement("div");
-    div.setAttribute("class", "compute");
-    div.setAttribute("id", "testing");
-    document.getElementById("test").appendChild(div);
-  };
-  useEffect(() => {
-    let down = document.getElementById("testing");
-    down.innerhtml = "";
-    down.removeAttribute("class");
-    down.setAttribute("class", "compute");
-    document.getElementById("test").appendChild(down);
+
+  const loadCells = () => {
+    console.log("creating 100 cells");
+    for (let i = 0; i < 100; i++) {
+      let div = document.createElement("div");
+      div.setAttribute("class", "compute");
+      div.style.display = "none";
+      document.getElementById("mainDiv").appendChild(div);
+    }
 
     window.sagecell.makeSagecell({
       inputLocation: "div.compute",
       evalButtonText: "Evaluate",
       linked: true,
     });
-    console.log("called");
-    
-    down.innerhtml = "";
-  }, [cells]);
+  };
+
+  // should only load this function once
+  useEffect(() => {
+    loadCells();
+
+    // not sure why the initial value will be 102, thats why i set the cellPos to 2
+    var c = document.getElementById("mainDiv").childElementCount;
+    console.log("number of cells created: " + c);
+  }, []);
 
   return (
     <React.Fragment>
-      <div id="test">
-        Type your own Sage computation below and click “Evaluate”.
-        <div class="compute" id="testing"></div>
-        <div class="compute" id="testing"></div>
-      </div>
       <button onClick={add}>Add new cell</button>
+      <div id="mainDiv">
+        Type your own Sage computation below and click “Evaluate”.
+        <div class="compute"></div>
+        <br></br>
+      </div>
     </React.Fragment>
   );
 };
