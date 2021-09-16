1d9aa4cc43b8e88bc9e8afecb4f92126c0a6dfe0
weijiepris <altxmare@hotmail.com>
2021-09-16 10:11:46 +0800

    uploading of data
    now it is required to have a dataset in order to upload to firebase.
    
    included some ui to show that data set has been selected and show the data transferred %

--- src/components/SageCells.js
+++ src/components/SageCells.js
@@ -5,11 +5,12 @@ import firebase from "firebase";
 const firebaseConfig = {
   apiKey: "AIzaSyCJO9dx2KjDfQvDHkpoxG3kZ49QyeiJGTc",
   authDomain: "bigdocker-csci321.firebaseapp.com",
-  databaseURL: "https://bigdocker-csci321-default-rtdb.asia-southeast1.firebasedatabase.app",
+  databaseURL:
+    "https://bigdocker-csci321-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "bigdocker-csci321",
   storageBucket: "bigdocker-csci321.appspot.com",
   messagingSenderId: "258512757467",
-  appId: "1:258512757467:web:18fd0326ed6097963a62fe"
+  appId: "1:258512757467:web:18fd0326ed6097963a62fe",
 };
 
 if (firebase.apps.length === 0) {
@@ -30,18 +31,18 @@ const SageCells = () => {
     if (cellPos < 99) {
       // increase the index by 1
       setCellPos(cellPos + 1);
-    }
-    else {
-      console.log("Can't have more than 100 cells (this is a limitation by us)!");
+    } else {
+      console.log(
+        "Can't have more than 100 cells (this is a limitation by us)!"
+      );
     }
   };
 
   const removeCell = () => {
     //If you have more than 1 cell...
-    if(cellPos > 1) {
+    if (cellPos > 1) {
       setCellPos(cellPos - 1);
-    }
-    else {
+    } else {
       console.log("Can't remove last cell!");
     }
   };
@@ -50,10 +51,9 @@ const SageCells = () => {
     for (let i = 0; i < numberOfCells; i++) {
       let div = document.createElement("div");
       div.setAttribute("class", "compute");
-      if(i === 0) {
+      if (i === 0) {
         div.style.display = "block";
-      }
-      else {
+      } else {
         div.style.display = "none";
       }
       document.getElementById("cellHolder").appendChild(div);
@@ -67,36 +67,39 @@ const SageCells = () => {
   };
 
   const jumpToLast = () => {
-    console.log("Jumping to cell number..." + (cellPos - 1) + ". Recall that cells start from 0!");
+    console.log(
+      "Jumping to cell number..." +
+        (cellPos - 1) +
+        ". Recall that cells start from 0!"
+    );
     let nodes = document.getElementById("cellHolder").children;
-    nodes[cellPos - 1].scrollIntoView({behavior:'smooth'});
+    nodes[cellPos - 1].scrollIntoView({ behavior: "smooth" });
   };
 
   const jumpToTop = () => {
-    document.documentElement.scrollIntoView({behavior:'smooth'});
+    document.documentElement.scrollIntoView({ behavior: "smooth" });
   };
 
   // consider this a init function
   useEffect(() => {
     loadCells(100);
-  }, []); //this is empty so no dependancy
 
+    console.log(uploading);
+  }, []); //this is empty so no dependancy
 
   //this is called whenever cellPos is updated
   useEffect(() => {
     console.log("I have " + cellPos + " cells");
     let nodes = document.getElementById("cellHolder").children;
     //technically slow implementation luckily its not heavy
-    for(let i = 0; i < 100; i++) {
+    for (let i = 0; i < 100; i++) {
       if (i <= cellPos) {
         nodes[i].style.display = "block";
-      }
-      else {
+      } else {
         nodes[cellPos].style.display = "none";
       }
-    };
-  },[cellPos]);
-
+    }
+  }, [cellPos]);
 
   const onUpload = () => {
     var input = document.createElement("input");
@@ -108,31 +111,42 @@ const SageCells = () => {
       reader = new FileReader();
       reader.onload = function () {
         document.getElementById("result").src = reader.result;
+        document.getElementById("result").value = reader.result;
+        console.log(reader);
         setUpdate(true);
       };
       reader.readAsDataURL(temp[0]);
     };
     input.click();
+    setUploading(true);
   };
 
-  const onChangeFile = async() => {
-    setUploading(true);
-    let task = storage.ref().child(Math.random().toString(36)).put(files[0]);
+  const onChangeFile = async () => {
+    if (!uploading) {
+      document.getElementById("progress").innerHTML =
+        "Please select a dataset to upload first.";
+      document.getElementById("result").focus();
+    } else {
+      let task = storage.ref().child(Math.random().toString(36)).put(files[0]);
 
-    const taskProgress = (snapshot) => {
-      console.log(`transferred: ${snapshot.bytesTransferred}`);
-    };
+      const taskProgress = (snapshot) => {
+        document.getElementById("progress").innerHTML = `Transferred: ${
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        }%`;
+        console.log(`transferred: ${snapshot.bytesTransferred}`);
+      };
 
-    const taskCompleted = () => {
-      task.snapshot.ref.getDownloadURL().then((snapshot) => {
-        savePostData(snapshot);
+      const taskCompleted = () => {
+        task.snapshot.ref.getDownloadURL().then((snapshot) => {
+          savePostData(snapshot);
+          console.log(snapshot);
+        });
+      };
+      const taskError = (snapshot) => {
         console.log(snapshot);
-      });
-    };
-    const taskError = (snapshot) => {
-      console.log(snapshot);
-    };
-    task.on("state_changed", taskProgress, taskError, taskCompleted);
+      };
+      task.on("state_changed", taskProgress, taskError, taskCompleted);
+    }
   };
   const savePostData = (profilefile) => {
     console.log(profilefile);
@@ -142,8 +156,11 @@ const SageCells = () => {
     <React.Fragment>
       <input placeholder="Upload Dataset" onClick={onUpload} id="result" />
       <button onClick={onChangeFile}>Upload Dataset </button>
+      <span id="progress"></span>
+      <br></br>
       <br></br>
       <span id="link"></span>
+      <br></br>
       <br></br>
       Type your own computation below and click “Evaluate”.
       <div class="btn-group">
@@ -158,7 +175,7 @@ const SageCells = () => {
           <button>Export notebook</button>
         </div>
       </div>
-      <div className={classes.container}id="cellHolder"></div>
+      <div className={classes.container} id="cellHolder"></div>
       <button onClick={jumpToTop}>Top</button>
     </React.Fragment>
   );
