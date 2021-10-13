3d485065cf3c215473464d45295feb747738fb6b
Zheng Qingping <qingping1998@gmail.com>
2021-10-13 20:51:45 +0800

    Add moon icon
    Small push, done for the day, hopefully nothing breaks, contact me if anything doesnt work

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,17 +1,18 @@
 import "../App.css";
 import "../css/Main.css";
-import {FaReact, FaDocker} from "react-icons/fa";
+import {FaReact, FaDocker, FaGithub} from "react-icons/fa";
 
 const Main = () => {
   return (
     <div className="page-content">
         <div className="mainText">
-          <h1>Welcome to BigDocker!</h1>
+          <h1>Welcome to BigDocker</h1>
+          <br/>
           <p>New to the app?</p>
           <button className="getStarted">Get Started</button>
         </div>
         <div className="poweredBy">
-          This app is powered by:
+          Powered by
           <div className="iconHolder">
             <a href="https://www.docker.com/" target="_blank" rel="noreferrer" className="icon">
               <FaDocker size={64} color="DodgerBlue"/>
@@ -20,6 +21,10 @@ const Main = () => {
             <a href="https://reactjs.org/" target="_blank" rel="noreferrer" className="icon">
               <FaReact size={64} color="Cyan"/>
               <p>React</p>
+            </a>
+            <a href="https://github.com/" target="_blank" rel="noreferrer" className="icon">
+              <FaGithub size={64}/>
+              <p>Github</p>
             </a>
             <a href="https://www.sagemath.org/" target="_blank" rel="noreferrer" className="icon">
               <img src="https://github.com/sagemath/website/blob/master/src/pix/icon_only/sagemath-icon-64.png?raw=true" alt="Sagemath"/>
